05dda4a9084695c3c8b012d357207a1403f369f2
Frédéric Bolvin <frederic@bol.vin>
2019-11-22 12:45:07 +0100

    Implemented seeding

--- src/seed.ts
+++ src/seed.ts
@@ -1,28 +1,28 @@
-// import { NestFactory } from '@nestjs/core';
-// import { Logger } from '@nestjs/common';
-// import { SeederModule } from './database/seeders/seeder.module';
-// import { Seeder } from './database/seeders/seeder';
+import { NestFactory } from '@nestjs/core';
+import { Logger } from '@nestjs/common';
+import { SeederModule } from './database/seeders/seeder.module';
+import { Seeder } from './database/seeders/seeder';
 
-// async function bootstrap() {
-//   NestFactory.createApplicationContext(SeederModule)
-//     .then(appContext => {
-//       const logger = appContext.get(Logger);
-//       const seeder = appContext.get(Seeder);
+async function bootstrap() {
+  NestFactory.createApplicationContext(SeederModule)
+    .then(appContext => {
+      const logger = appContext.get(Logger);
+      const seeder = appContext.get(Seeder);
 
-//       seeder
-//         .seed()
-//         .then(() => {
-//           logger.log('Seeding complete!');
-//         })
-//         .catch(error => {
-//           logger.error('Seeding failed!');
-//           throw error;
-//         })
-//         .finally(() => appContext.close());
-//     })
-//     .catch(error => {
-//       throw error;
-//     });
-// }
+      seeder
+        .seed()
+        .then(() => {
+          logger.log('Seeding complete!');
+        })
+        .catch(error => {
+          logger.error('Seeding failed!');
+          throw error;
+        })
+        .finally(() => appContext.close());
+    })
+    .catch(error => {
+      throw error;
+    });
+}
 
-// bootstrap();
+bootstrap();
